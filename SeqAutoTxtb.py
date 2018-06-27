13b31cff6043dc18676213df59d0c802f1fd4729
Superpflaume <38477227+Superpflaume@users.noreply.github.com>
2018-06-27 01:01:26 +0100

    s fix

--- SeqAutoTxtb.py
+++ SeqAutoTxtb.py
@@ -19,18 +19,21 @@ for i in fasta_files:
     fasta_string = open(i).read() #or make the names fasta1.fasta and just do open(i).read
     result_handle = NCBIWWW.qblast("blastn", "nr", fasta_string, hitlist_size=5)
 
+    print result_handle
     #blast_records = NCBIXML.parse(result_handle) 
     blast_records = NCBIXML.read(result_handle)# if you only have one seq in file
-    E_VALUE_THRESH = 0.1
-    for blast_record in blast_records:
-        for alignment in blast_record.alignments:
-            for hsp in alignment.hsps:
-                if hsp.expect < E_VALUE_THRESH:
-                    file_string += "alignment:",alignment.title+"\n"
-                    file_string += "e-value:",hsp.expect+"\n"
+    print blast_records
+    
+    E_VALUE_THRESH = 0.001
+    #for blast_record in blast_records:
+    for alignment in blast_records.alignments:
+        for hsp in alignment.hsps:
+            if hsp.expect < E_VALUE_THRESH:
+                file_string += "alignment:",alignment.title+"\n"
+                file_string += "e-value:",hsp.expect+"\n"
     x += 1
     File.write(file_string)
-    print "file"(i)
+    print "file" 
 
 end = time.time()
 print(end - start)
