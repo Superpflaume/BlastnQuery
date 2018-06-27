6d427d7089b5bc8b41a1fecaf1ca66db80f4a27c
Superpflaume <38477227+Superpflaume@users.noreply.github.com>
2018-06-27 13:22:35 +0100

    final

--- SeqAutoTxtb.py
+++ SeqAutoTxtb.py
@@ -25,18 +25,13 @@ with open("output"+str(x)+".txt","w") as File:
         result_handle = NCBIWWW.qblast("blastn", "nr", fasta_string, hitlist_size=5)
 
 
-        #blast_records = NCBIXML.parse(result_handle) 
         blast_records = NCBIXML.read(result_handle)# if you only have one seq in file
 
-
-        #E_VALUE_THRESH = 0.001
-        #for blast_record in blast_records:
         for alignment in blast_records.alignments:
-            #for hsp in alignment.hsps:
-                #if hsp.expect < E_VALUE_THRESH:
-            file_string += "File:" + str(i) + "\n"
-            file_string += "alignment:"+ str(alignment.title)+"\n"
-            file_string += "e-value:"+ str(hsp.expect)+"\n"
+            for hsp in alignment.hsps:
+                file_string += "File:" + str(i) + "\n"
+                file_string += "alignment:"+ str(alignment.title)+"\n"
+                file_string += "e-value:"+ str(hsp.expect)+"\n"
         x += 1
         File.write(file_string)
         print i
