a905ac79e7907b5374cb5924e50feddcd6747504
Superpflaume <38477227+Superpflaume@users.noreply.github.com>
2018-07-31 15:02:39 +0100

    leider nur 1 datei

--- SmartBlast.py
+++ SmartBlast.py
@@ -7,35 +7,37 @@ import time
 import csv
 from itertools import izip
 
-start = time.time()
+x=1
 
-with open("BLASToutputPARSED.xml", "w") as output:
+with open("BLASToutputPARSED"+str(x)+".xml", "w") as output:
     for file in glob.glob("*.txt"):
         fasta_sequence = open(file,"r").read()
         if fasta_sequence.startswith(">"):            
             print file , "blasting..."
+            start = time.time()
+            
+            
             result_handle = NCBIWWW.qblast("blastn", "nr", fasta_sequence, hitlist_size=5)
-
             output.write(result_handle.read())
-            result_handle.close()
-       
-            print file, "done"
+            #result_handle.close()
 
             end = time.time()
             print(end - start)
+            x += 1
             
-result_handle = open ("BLASToutputPARSED.xml","r")
-blast_records = NCBIXML.parse(result_handle)
-blast_record = next(blast_records)
+#result_handle = open ("BLASToutputPARSED.xml","r")
+            #blast_records = NCBIXML.parse(result_handle)
+            blast_records = NCBIXML.read(result_handle)
+            blast_record = next(blast_records)
 
-with open("BLAST.csv", "w") as output:
-    writer = csv.writer(output, delimiter = ";")
-    writer.writerow(["File ","Title", "Length","E-value"])
-    for alignment in blast_record.alignments:
-        for hsp in alignment.hsps:
-            output.write(str(file) + ";")
-            output.write(str(alignment.title)+ ";")
-            output.write(str(alignment.length)+ ";")
-            output.write(str(hsp.expect) + "\n")
+            with open("BLAST.csv", "w") as output_csv:
+                writer = csv.writer(output_csv, delimiter = ";")
+                writer.writerow(["File ","Title", "Length of alignment","E-value"])
+                for alignment in blast_record.alignments:
+                    for hsp in alignment.hsps:
+                        output_csv.write(str(file) + ";")
+                        output_csv.write(str(alignment.title)+ ";")
+                        output_csv.write(str(alignment.length)+ ";")
+                        output_csv.write(str(hsp.expect) + "\n")
         
 
--- smartXML.py
+++ smartXML.py
@@ -7,7 +7,7 @@ result_handle = open ("BLASToutput.xml")
 blast_records = NCBIXML.parse(result_handle)
 blast_record = next(blast_records)
 
-with open("BLAST.csv", "w") as output:
+with open("BLAST2.csv", "w") as output:
     writer = csv.writer(output, delimiter = ';')
     writer.writerow(['File ','Title', 'Length','E-value'])
     for alignment in blast_record.alignments:
