7c37a0fbea6fe6a76702991b11d9344b0117df5e
Superpflaume <38477227+Superpflaume@users.noreply.github.com>
2018-07-31 14:04:13 +0100

    almosssst

--- SmartBlast.py
+++ SmartBlast.py
@@ -10,9 +10,9 @@ from itertools import izip
 start = time.time()
 
 with open("BLASToutputPARSED.xml", "w") as output:
-    for file in glob.glob('*.txt'):
-        fasta_sequence = open(file,'r').read()
-        if fasta_sequence.startswith('>')==True:            
+    for file in glob.glob("*.txt"):
+        fasta_sequence = open(file,"r").read()
+        if fasta_sequence.startswith(">"):            
             print file , "blasting..."
             result_handle = NCBIWWW.qblast("blastn", "nr", fasta_sequence, hitlist_size=5)
 
@@ -21,26 +21,21 @@ with open("BLASToutputPARSED.xml", "w") as output:
        
             print file, "done"
 
-end = time.time()
-print(end - start)
+            end = time.time()
+            print(end - start)
             
-result_handle = open ("BLASToutputPARSED.xml")
+result_handle = open ("BLASToutputPARSED.xml","r")
 blast_records = NCBIXML.parse(result_handle)
 blast_record = next(blast_records)
 
 with open("BLAST.csv", "w") as output:
-    output.write('File ;')
-    output.write('Title;')
-    output.write('Length;')
-    output.write('E-value \n')
+    writer = csv.writer(output, delimiter = ";")
+    writer.writerow(["File ","Title", "Length","E-value"])
     for alignment in blast_record.alignments:
         for hsp in alignment.hsps:
-            fil = str(file) + ';' 
-            tit = str(alignment.title)+ ';'
-            leng = str(alignment.length)+ ';' 
-            val = str(hsp.expect) + '\n'
+            output.write(str(file) + ";")
+            output.write(str(alignment.title)+ ";")
+            output.write(str(alignment.length)+ ";")
+            output.write(str(hsp.expect) + "\n")
+        
 
-            output.write(fil)
-            output.write(tit)
-            output.write(leng)
-            output.write(val)
--- smartXML.py
+++ smartXML.py
@@ -7,24 +7,13 @@ result_handle = open ("BLASToutput.xml")
 blast_records = NCBIXML.parse(result_handle)
 blast_record = next(blast_records)
 
-header = ['File ;','Title;', 'Length;','E-value \n']
-i = 0
 with open("BLAST.csv", "w") as output:
-    writer = csv.writer(output)
-    writer.writerow(i for i in header)
-    #output.write(i for i in header)
+    writer = csv.writer(output, delimiter = ';')
+    writer.writerow(['File ','Title', 'Length','E-value'])
     for alignment in blast_record.alignments:
         for hsp in alignment.hsps:
-            fil = str(file) + ';' 
-            tit = str(alignment.title)+ ';'
-            leng = str(alignment.length)+ ';' 
-            val = str(hsp.expect)+ ';' + '\n'
-
-            output.write(fil)
-            output.write(tit)
-            output.write(leng)
-            output.write(val)
-
-    
-    #writer.writerow(('File', 'Alignment', 'E-value'))
-#writer.writerows(izip(fil, ali, e))
+            output.write(str(file) + ';')
+            output.write(str(alignment.title)+ ';')
+            output.write(str(alignment.length)+ ';')
+            output.write(str(hsp.expect) + '\n')
+        
