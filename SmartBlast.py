ba7ac4a53de4cc47fbcba21bd7c151d264468bc4
Superpflaume <38477227+Superpflaume@users.noreply.github.com>
2018-07-31 11:35:57 +0100

    useless

--- SmartBlast.py
+++ SmartBlast.py
@@ -9,29 +9,27 @@ from itertools import izip
 
 start = time.time()
 
-with open("BLASToutput.xml", "w") as output:
+with open("BLASToutputPARSED.xml", "w") as output:
     for file in glob.glob('*.txt'):
         fasta_sequence = open(file,'r').read()
         if fasta_sequence.startswith('>')==True:            
-            print file
             result_handle = NCBIWWW.qblast("blastn", "nr", fasta_sequence, hitlist_size=5)
-    
-            output.write(result_handle.read())
-                #changeneeded
+
+            blast_records = NCBIXML.read(result_handle)
+
+            for alignment in blast_records.alignments:
+                for hsp in alignment.hsps:
+                    output.write(file)
+                    output.write("sequence:", alignment.title)
+                    output.write("length:", alignment.length)
+                    output.write("e value:", hsp.expect)
+
             result_handle.close()
        
             print"done:", file
 
-            
-result_handle = open("BLASToutput.xml")
-blast_records = NCBIXML.parse(result_handle)
-
-for alignment in blast_records.alignments:
-    for hsp in alignment.hsps:
-        print("****Alignment****")
-        print("sequence:", alignment.title)
-        print("length:", alignment.length)
-        print("e value:", hsp.expect)
-
 end = time.time()
 print(end - start)
+            
+
+
