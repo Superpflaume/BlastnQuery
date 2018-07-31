7429205dc9613aec88f8fc1ecce8d09fe4a626d5
Superpflaume <38477227+Superpflaume@users.noreply.github.com>
2018-07-31 12:14:16 +0100

    better

--- smartXML.py
+++ smartXML.py
@@ -8,15 +8,16 @@ blast_records = NCBIXML.parse(result_handle)
 blast_record = next(blast_records)
 
 with open("BLAST.csv", "w") as output:
+    output.write('File ;')
+    output.write('Title;')
+    output.write('Length;')
+    output.write('E-value \n')
     for alignment in blast_record.alignments:
         for hsp in alignment.hsps:
             fil = str(file) + ';' 
             tit = str(alignment.title)+ ';'
             leng = str(alignment.length)+ ';' 
             val = str(hsp.expect)+ ';' + '\n'
-
-            writer = csv.writer(output)
-            writer.writerow(('File', 'Title', 'Length', 'E-value'))
 
             output.write(fil)
             output.write(tit)
