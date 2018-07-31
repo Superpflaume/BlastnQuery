8d328e7e70766bb456c658df4e83d86f4b633097
Superpflaume <38477227+Superpflaume@users.noreply.github.com>
2018-07-31 11:09:09 +0100

    appreciatethexml

--- SmartBlast.py
+++ SmartBlast.py
@@ -9,21 +9,29 @@ from itertools import izip
 
 start = time.time()
 
-with open("BLASToutput.csv", "w") as output:
+with open("BLASToutput.xml", "w") as output:
     for file in glob.glob('*.txt'):
         fasta_sequence = open(file,'r').read()
-        if fasta_sequence.startswith('>')==True:
+        if fasta_sequence.startswith('>')==True:            
             print file
-            
             result_handle = NCBIWWW.qblast("blastn", "nr", fasta_sequence, hitlist_size=5)
     
             output.write(result_handle.read())
                 #changeneeded
             result_handle.close()
        
-            print "next"
+            print"done:", file
 
+            
+result_handle = open("BLASToutput.xml")
+blast_records = NCBIXML.parse(result_handle)
 
+for alignment in blast_records.alignments:
+    for hsp in alignment.hsps:
+        print("****Alignment****")
+        print("sequence:", alignment.title)
+        print("length:", alignment.length)
+        print("e value:", hsp.expect)
 
 end = time.time()
 print(end - start)
