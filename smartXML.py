a79308719bcf618b81b2e0611e6ed865e690c8e8
Superpflaume <38477227+Superpflaume@users.noreply.github.com>
2018-07-31 12:07:33 +0100

    header wrong

--- smartXML.py
+++ smartXML.py
@@ -1,19 +1,24 @@
 #SmartXML
 from Bio.Blast import NCBIXML
 import csv
+from itertools import izip
 
 result_handle = open ("BLASToutput.xml")
 blast_records = NCBIXML.parse(result_handle)
 blast_record = next(blast_records)
 
-with open("BLASToutputPARSED.csv", "w") as output:
+with open("BLAST.csv", "w") as output:
     for alignment in blast_record.alignments:
         for hsp in alignment.hsps:
-            a = str(file) + ';' 
-            b = "sequence:" + str(alignment.title)+ ';'
-            c = "length:" + str(alignment.length)+ ';' 
-            d = "e value:" + str(hsp.expect)+ ';' + '\n'
-            output.write(a)
-            output.write(b)
-            output.write(c)
-            output.write(d)
+            fil = str(file) + ';' 
+            tit = str(alignment.title)+ ';'
+            leng = str(alignment.length)+ ';' 
+            val = str(hsp.expect)+ ';' + '\n'
+
+            writer = csv.writer(output)
+            writer.writerow(('File', 'Title', 'Length', 'E-value'))
+
+            output.write(fil)
+            output.write(tit)
+            output.write(leng)
+            output.write(val)
