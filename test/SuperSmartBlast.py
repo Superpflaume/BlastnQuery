90456147498da9bc8e8b2b4c2d7921fc67452f13
Superpflaume <38477227+Superpflaume@users.noreply.github.com>
2018-08-02 11:28:34 +0100

    comments

--- test/SuperSmartBlast.py
+++ test/SuperSmartBlast.py
@@ -1,34 +1,32 @@
 #supersmartblast
 
-from Bio.Blast import  NCBIWWW
-from Bio.Blast import NCBIXML
-import glob
-import time
-import csv
-from itertools import izip
+from Bio.Blast import  NCBIWWW # for online access to NCBI Blast
+from Bio.Blast import NCBIXML # to read the Blast-results
+import glob # to find all files in one folder
+import csv # to write a csv which can be displayed by MS Excel
 
 x=1
 
-for file in glob.glob("*.txt"):
-    with open("BlastnSeq"+str(x)+".xml", "w") as output_xml:
-        fasta_sequence = open(file,"r").read()
-        if fasta_sequence.startswith(">"):
-            print file , "blasting..."
+for file in glob.glob("*.txt"):    # find all TXT-files in the folder, therefor all FASTA-sequences
+    with open("BlastnSeq"+str(x)+".xml", "w") as output_xml: # generate an empty XML-document, one for each sequences results
+        fasta_sequence = open(file,"r").read() # open FASTA-file and save sequence in variable "fasta_sequence"
+        if fasta_sequence.startswith(">"): # eliminate all empty files
+            print file , "blasting..." # status update
 
-            result_handle = NCBIWWW.qblast("blastn", "nr", fasta_sequence, hitlist_size=5)
-            output_xml.write(result_handle.read())
-            result_handle.close()
+            result_handle = NCBIWWW.qblast("blastn", "nr", fasta_sequence, hitlist_size=5) # upload the sequence to NCBI Blast database, perform a "blastn" search for "somewhat similar sequences" to get results for every sample, and download the first 5 hits (to keep the results managable)
+            output_xml.write(result_handle.read()) # write the results in the previously opened XML file
+            result_handle.close() # close the previous search
 
-            x += 1
+            x += 1 # count up for the next XML-file to be opened
 
-with open("AutoBlastn.csv", "wb") as output_csv:
-    writer = csv.writer(output_csv, delimiter = ';')
-    writer.writerow(['File ','Title', 'Length','E-value'])
-    for one_xml in glob.glob("*.xml"):
-        with open(one_xml, "r") as result_handle:
-            blast_records = NCBIXML.parse(result_handle)
-            blast_record = next(blast_records)
+with open("AutoBlastn.csv", "wb") as output_csv: # generate an empty CSV-file to store the desired information about all sequences
+    writer = csv.writer(output_csv, delimiter = ';')  # define a method how the data is to be written in the document
+    writer.writerow(['File ','Title', 'Length','E-value']) # put a header for the document
+    for one_xml in glob.glob("*.xml"): # find all XML-files in the folder that were previously generated
+        with open(one_xml, "r") as result_handle: # open one XML-file after another and save information in "result_handle"
+            blast_records = NCBIXML.parse(result_handle) # decrypt information from XML-file and make it accessible line by line
+            blast_record = next(blast_records) # move trough lines ofXML-file
 
-            for alignment in blast_record.alignments:
-                for hsp in alignment.hsps:
-                    writer.writerow([str(file), str(alignment.title), str(alignment.length), str(hsp.expect)])
+            for alignment in blast_record.alignments: # any line in the XML-file that...
+                for hsp in alignment.hsps: # ... includes information on the hit such as...
+                    writer.writerow([str(file), str(alignment.title), str(alignment.length), str(hsp.expect)]) # name of the TXT-file that was blasted, title of alignment, length of alignment and e-values of alignment is to be written in the CSV-file.
